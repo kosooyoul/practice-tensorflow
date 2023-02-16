a3ebca3fdb02c00187746a9e6b7a41b5cebe7f8f
kosooyoul <kosooyoul@gmail.com>
2023-02-16 23:43:26 +0900

    Upt. add test model, but does not work

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import * as tf from '@tensorflow/tfjs';
 
 const SEQ_LENGTH = 5; // 입력 시퀀스 길이
 
+async function testModel(model: tf.LayersModel, testText: string) {
+  const testSequence = testText.split('').map(c => c.charCodeAt(0) / 255);
+  const xTest = tf.tensor2d([testSequence], [1, SEQ_LENGTH]);
+
+  const prediction = model.predict(xTest) as tf.Tensor;
+  const predictedValue = (await prediction.data())[0] * 255;
+  const predictedChar = String.fromCharCode(predictedValue);
+
+  return predictedChar;
+}
+
 async function main() {
   // 딥러닝 모형 정의
   const model = tf.sequential();
-  model.add(tf.layers.embedding({ inputDim: 20000, outputDim: 300, inputLength: 5 }));
+  model.add(tf.layers.embedding({ inputDim: 20000, outputDim: 300, inputLength: SEQ_LENGTH }));
   model.add(tf.layers.lstm({ units: 200, inputShape: [SEQ_LENGTH, 1], returnSequences: false }));
   model.add(tf.layers.dense({ units: 2, activation: 'softmax' }));
   model.compile({ optimizer: 'adam', loss: 'sparseCategoricalCrossentropy', metrics: ['accuracy'] });
@@ -33,6 +44,11 @@ async function main() {
   const history = await model.fit(x, y, { epochs: 10, batchSize: 128 });
 
   console.log(history);
+
+  console.log('start test');
+  const result = await testModel(model, 'apple');
+
+  console.log('test result is ' + result);
 }
 
 main();
